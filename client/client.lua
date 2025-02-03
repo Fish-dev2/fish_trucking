6ca19fe913b06923bdca70c1a49537711dfa68b6
Balint "Fish" Fuzi <capn.fuzi.balint@gmail.com>
2025-02-03 23:41:24 +0100

    Teleporting and waypoint reading added
    This function will be made into a separate script later, basically I added a teleport function that teleports the player to the ground-appropriate z coord.

--- client/client.lua
+++ client/client.lua
@@ -9,6 +9,10 @@ else
 end
 
 
+
+--first i should add teleporting and waypoint reading
+
+
 RegisterCommand('truck', function(_,args)
     local targetId = args[1]
 
@@ -26,3 +30,59 @@ RegisterCommand('trailer', function(_,args)
 
 
 end)
+
+
+
+
+
+RegisterCommand('+teleportwaypoint', function(_,args)
+    local playerped = PlayerPedId()
+    local waypoint = GetFirstBlipInfoId(8) -- 8 is the ID for waypoints
+    if DoesBlipExist(waypoint) then
+        local x, y, z = table.unpack(GetBlipCoords(waypoint))
+
+        local groundZ = nil
+        local retval = nil
+        for i=0, 1000.0, 75.0 do
+            StartPlayerTeleport(PlayerId(), x, y, i, 0.0, false, false, true)
+
+            --Wait(100)
+            while IsPlayerTeleportActive() do
+                Citizen.Wait(0)
+            end
+
+            retval, groundZ = GetGroundZFor_3dCoord(x, y, i, true)
+
+            if retval then
+                break
+            end
+        end
+        --Wait(300)
+        
+        print(retval)
+        print(groundZ)
+
+        StartPlayerTeleport(PlayerId(), x, y, groundZ+5.0, 0.0, false, true, true)
+        while IsPlayerTeleportActive() do
+            Citizen.Wait(0)
+        end
+
+        TriggerEvent('chat:addMessage', {
+            args = {'Waypoint Coords: ', coords}
+        })
+
+    else
+        TriggerEvent('chat:addMessage', {
+            args = {'No waypoint set.',}
+        })
+    end
+end)
+
+RegisterCommand('getcoords', function(_,args)
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    print(playerCoords)
+end)
+
+
+
+RegisterKeyMapping('+teleportwaypoint', 'Teleport To Marked Waypoint', 'keyboard', 'PAGEUP')
